9c5ef639605b7d67d54f449dc11cb343fcf36485
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-11-29 23:43:26 +0300

    Refactor. Drop constants moved to db_const.php. Fix alignment.

--- sys/db.php
+++ sys/db.php
@@ -4,32 +4,12 @@
   * @author Mikhail Veltishchev <dichlofos-mv@yandex.ru>
   * Database management module
   **/
-require_once("${xengine_dir}sys/string.php");
-require_once("${xengine_dir}sys/auth.php");
+global $full_xengine_dir;
+global $xengine_dir;
 
-define('XDB_NEW', 'new');
-define('XDB_APPROVED_NEW', 'approved_new');
-define('XDB_INVALID_ID', '-1');
-
-define('XDB_OVERRIDE_TS', true);
-define('XDB_NO_OVERRIDE_TS', false); // default
-
-define('XDB_USE_AI', true);
-define('XDB_NO_USE_AI', false); // default
-
-define('XDB_DEBUG_AREA_DISABLED', false);
-define('XDB_DEBUG_AREA_ENABLED', true); // default
-
-define('XDB_DB_NAME', 'db_name');
-define('XDB_DEFAULT_DB_NAME', "ank/fizlesh.sqlite3");
-
-define('XDB_DB_TYPE', 'db_type');
-
-define('XDB_DB_TYPE_SQLITE3', "sqlite3");
-define('XDB_DB_TYPE_PG', "pg");
-
-define('XDB_DEFAULT_DB_TYPE', XDB_DB_TYPE_SQLITE3);
-
+require_once("${full_xengine_dir}sys/db_const.php");
+require_once("${full_xengine_dir}sys/string.php");
+require_once("${full_xengine_dir}sys/auth.php");
 
 /**
   * Helper for obtaining keys that are DB ids from request. Filters all
--- sys/settings.php
+++ sys/settings.php
@@ -1,40 +1,45 @@
 <?php
-    require_once("settings.php");
-    global $SETTINGS;
-    global $engine_dir;
-    global $xengine_dir;
+require_once("settings.php");
+global $SETTINGS;
+global $engine_dir;
+global $xengine_dir;
+global $full_xengine_dir;
+if (strlen($full_xengine_dir) == 0) {
+    die("Complete path to xengine directory (`full_xengine_dir`) was not set properly. Please check your `settings.php`. ");
+}
 
-    $SETTINGS["defaultpage"] = "index";
-    $SETTINGS["nopagecode"] = "error.404";
-    $SETTINGS["openbracket"] = "<# ";
-    $SETTINGS["closebracket"] = "#>";
-    $SETTINGS["xengine_dir"] = $xengine_dir;
-    $SETTINGS["engine_dir"] = $engine_dir;
-    $SETTINGS["engine_pub"] = $engine_pub;
-    $SETTINGS["documentsdir"] = $design_dir;
-    $SETTINGS["design_dir"] = $design_dir;
-    $SETTINGS["content_dir"] = $content_dir;
-    $SETTINGS["precdir"] = ".prec/";
-    $SETTINGS["code_begin"] = '<?php unset($argv);unset($param);';
-    $SETTINGS["code_end"] = '?>';
-    $SETTINGS["mailer_enabled"] = $mailer_enabled;
-    // auth session lifetime in seconds
-    $SETTINGS["session_time"] = 7*24*3600;
-    // redirect delay in seconds (for autotesting needs)
-    $SETTINGS["zero_redirect_delay"] = "0";
-    // content versioning roundup
-    $SETTINGS["content_time_roundup"] = 100;
+global $mailer_enabled;
 
-    // set up some common-used things
-    global $full_content_dir;
-    global $full_design_dir;
-    global $full_engine_pub;
+$SETTINGS["defaultpage"] = "index";
+$SETTINGS["nopagecode"] = "error.404";
+$SETTINGS["openbracket"] = "<# ";
+$SETTINGS["closebracket"] = "#>";
+$SETTINGS["xengine_dir"] = $xengine_dir;
+$SETTINGS["engine_dir"] = $engine_dir;
+$SETTINGS["engine_pub"] = $engine_pub;
+$SETTINGS["documentsdir"] = $design_dir;
+$SETTINGS["design_dir"] = $design_dir;
+$SETTINGS["content_dir"] = $content_dir;
+$SETTINGS["precdir"] = ".prec/";
+$SETTINGS["code_begin"] = '<?php unset($argv);unset($param);';
+$SETTINGS["code_end"] = '?>';
+$SETTINGS["mailer_enabled"] = $mailer_enabled;
+// auth session lifetime in seconds
+$SETTINGS["session_time"] = 7*24*3600;
+// redirect delay in seconds (for autotesting needs)
+$SETTINGS["zero_redirect_delay"] = "0";
+// content versioning roundup
+$SETTINGS["content_time_roundup"] = 100;
 
-    global $meta_site_url;
-    global $meta_site_url_secure;
-    $meta_site_url_secure = str_replace("http:", "https:", $meta_site_url);
+// set up some common-used things
+global $full_content_dir;
+global $full_design_dir;
+global $full_engine_pub;
 
-    $full_content_dir = "/$web_prefix${content_dir}";
-    $full_design_dir = "/$web_prefix${design_dir}";
-    $full_engine_pub = "/${web_prefix}{$SETTINGS["engine_pub"]}";
-?>
+global $meta_site_url;
+global $meta_site_url_secure;
+$meta_site_url_secure = str_replace("http:", "https:", $meta_site_url);
+
+$full_content_dir = "/$web_prefix${content_dir}";
+$full_design_dir = "/$web_prefix${design_dir}";
+$full_engine_pub = "/${web_prefix}{$SETTINGS["engine_pub"]}";
